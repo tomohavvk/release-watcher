99ac285a03edf5e8302dd699270a96474b798e62
Ihor Zadyra <i.zadyra@rozetkapay.com>
2026-07-05 22:15:13 +0300

    WIP

--- internal/db/migration/000002_add_telegram_fields.up.sql
+++ internal/db/migration/000002_add_telegram_fields.up.sql
@@ -1,4 +1,6 @@
-ALTER TABLE users ADD COLUMN telegram_chat_id INTEGER UNIQUE;
+ALTER TABLE users ADD COLUMN telegram_chat_id INTEGER;
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_users_telegram_chat_id ON users(telegram_chat_id) WHERE telegram_chat_id IS NOT NULL;
 
 CREATE TABLE IF NOT EXISTS user_repositories (
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
